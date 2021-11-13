3fa1da6115767194037613643a2dbc6eb74514aa
anggitrisi <anggitrisihombing@gmail.com>
2021-11-13 14:57:51 +0700

    add validation sign in

--- src/components/sign-in-sign-up/sign-in/Form.jsx
+++ src/components/sign-in-sign-up/sign-in/Form.jsx
@@ -4,22 +4,31 @@ import { StyledLogo } from "../../../styles/Navbar";
 import logo from "../../../images/Logo.png";
 import "../styles/Style.css";
 import Button from "../../UI/Button";
-const Form = () => {
+import SigninFunction from "../SigninFunction";
+import validate from "../ValidateInfo";
+const Form = (submitForm) => {
+  const { handleChange, values, handleSubmit, errors } = SigninFunction(submitForm, validate);
+
   return (
     <>
       <div className="forms-container">
         <div className="signin-signup">
-          <form action="" className="sign-in-form">
+          <form onSubmit={handleSubmit} className="sign-in-form">
             <StyledLogo src={logo} alt="Logo" />
-
-            <h2 className="title">Sign In</h2>
-            <div className="input-field">
-              <i className="fas fa-user"></i>
-              <input type="text" placeholder="Username" />
+            <div className="form-input">
+              <h2 className="title">Sign In</h2>
+              <div className="input-field">
+                <i className="fas fa-user"></i>
+                <input id="username" type="text" nama="username" placeholder="Username" value={values.username} onChange={handleChange} />
+              </div>
+              {errors.username && <p>{errors.username}</p>}
             </div>
-            <div className="input-field">
-              <i className="fas fa-lock"></i>
-              <input type="password" placeholder="Password" />
+            <div className="form-input">
+              <div className="input-field">
+                <i className="fas fa-lock"></i>
+                <input id="password" type="password" placeholder="Password" name="password" value={values.password} onChange={handleChange} />
+              </div>
+              {errors.password && <p>{errors.password}</p>}
             </div>
             <Button label="Sign in" />
 
